62d9a8a83ba156cf3f77dd2fbd92d529d32e507f
Rockford Wei <rocky@perfect.org>
2017-10-14 22:56:46 -0400

    Renovating the layout to Swift 4, completely.

--- Package.swift
+++ Package.swift
@@ -1,3 +1,5 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 //
 //  Package.swift
 //  PerfectCrypto
@@ -20,17 +22,59 @@
 import PackageDescription
 
 #if os(Linux)
-	let cOpenSSLRepo = "https://github.com/PerfectlySoft/Perfect-COpenSSL-Linux.git"
-#else
-	let cOpenSSLRepo = "https://github.com/PerfectlySoft/Perfect-COpenSSL.git"
-#endif
 
 let package = Package(
     name: "PerfectCrypto",
-    targets: [],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PerfectCrypto",
+            targets: ["PerfectCrypto"]),
+    ],
     dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 3),
-		.Package(url: cOpenSSLRepo, majorVersion: 3)
-	]
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+				.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
+				.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
+				.package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL-Linux.git", from: "3.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "PerfectCrypto",
+            dependencies: ["PerfectLib", "PerfectThread", "COpenSSL"]),
+        .testTarget(
+            name: "PerfectCryptoTests",
+            dependencies: ["PerfectCrypto"]),
+    ]
 )
+#else
+
+let package = Package(
+    name: "PerfectCrypto",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PerfectCrypto",
+            targets: ["PerfectCrypto"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+				.package(url: "https://github.com/PerfectlySoft/PerfectLib.git", from: "3.0.0"),
+				.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
+				.package(url: "https://github.com/PerfectlySoft/Perfect-COpenSSL.git", from: "3.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "PerfectCrypto",
+            dependencies: ["PerfectLib", "PerfectThread", "COpenSSL"]),
+        .testTarget(
+            name: "PerfectCryptoTests",
+            dependencies: ["PerfectCrypto"]),
+    ]
+)
+#endif
